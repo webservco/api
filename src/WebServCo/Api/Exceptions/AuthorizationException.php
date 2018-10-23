3d3f530d72703390ce522b37343b20d8b5d3511a
Radu Maier <ariademur@webserv.co>
2018-10-23 09:35:55 +0200

    AuthorizationException ERROR_CODE

--- src/WebServCo/Api/Exceptions/AuthorizationException.php
+++ src/WebServCo/Api/Exceptions/AuthorizationException.php
@@ -3,6 +3,8 @@ namespace WebServCo\Api\Exceptions;
 
 final class AuthorizationException extends AbstractSecurityException
 {
+    const ERROR_CODE = 'authorization_exception';
+
     public function __construct($message, $code = 401, \Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
