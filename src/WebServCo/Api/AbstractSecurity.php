7f1ed6c0455137d70dbf0e9f7db4b09e846f1419
Radu Maier <ariademur@webserv.co>
2018-10-23 18:32:21 +0200

    AbstractSecurity

--- src/WebServCo/Api/AbstractSecurity.php
+++ src/WebServCo/Api/AbstractSecurity.php
@@ -13,16 +13,43 @@ abstract class AbstractSecurity
     public function verify()
     {
         $this->verifySsl();
-        $this->verifyAuthorization();
+        $this->verifyMethod();
+        $this->verifyContentType();
     }
 
-    abstract protected function verifyAuthorization();
+    abstract protected function getSupportedContentTypes();
+
+    abstract protected function getAllowedMethods();
+
+    abstract public function verifyAuthorization();
+
+    protected function verifyContentType()
+    {
+        $clientContentTypes = $this->request->getAcceptContentTypes();
+        if (array_key_exists(0, $clientContentTypes)) {
+            unset($clientContentTypes[0]); // $q == 0 means, that mime-type isn’t supported!
+        }
+        $supportedContentTypes = $this->getSupportedContentTypes();
+        $intersection = array_intersect($clientContentTypes, $supportedContentTypes);
+        if (empty($intersection)) {
+            throw new \WebServCo\Framework\Exceptions\UnsupportedMediaTypeException('Unsupported content type');
+        }
+        return true;
+    }
+
+    protected function verifyMethod()
+    {
+        if (!in_array($this->request->getMethod(), $this->getAllowedMethods())) {
+            throw new \WebServCo\Framework\Exceptions\MethodNotAllowedException('Unsupported method');
+        }
+        return true;
+    }
 
     protected function verifySsl()
     {
         $schema = $this->request->getSchema();
         if ('https' != $schema) {
-            throw new \WebServCo\Api\Exceptions\SslException('SSL required');
+            throw new \WebServCo\Framework\Exceptions\SslRequiredException('SSL required');
         }
         return true;
     }
