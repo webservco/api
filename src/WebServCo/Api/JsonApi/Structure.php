3f64b39a492daec1f1a14ee21d575d9bf6ffe23c
Radu Maier <ariademur@webserv.co>
2018-10-30 17:59:35 +0100

    JsonApi/Structure improvements: toJson, VERSION, support multiple data objects

--- src/WebServCo/Api/JsonApi/Structure.php
+++ src/WebServCo/Api/JsonApi/Structure.php
@@ -9,18 +9,19 @@ class Structure
     protected $errors;
 
     const CONTENT_TYPE = 'application/vnd.api+json';
+    const VERSION = '1.0';
 
     public function __construct()
     {
         $this->meta = [];
-        $this->jsonapi = ['version' => '1.0'];
+        $this->jsonapi = ['version' => self::VERSION];
         $this->data = [];
         $this->errors = [];
     }
 
     public function setData(\WebServCo\Api\JsonApi\Interfaces\ResourceObjectInterface $resourceObject)
     {
-        $this->data = $resourceObject->toArray();
+        $this->data[] = $resourceObject;
     }
 
     public function setError(\WebServCo\Api\JsonApi\Error $error)
@@ -41,8 +42,21 @@ class Structure
                 $array['errors'][] = $error->toArray();
             }
         } else {
-            $array['data'] = $this->data;
+            $dataItems = count($this->data);
+            if (1 < $dataItems) {
+                foreach ($this->data as $item) {
+                    $array['data'][] = $item->toArray();
+                }
+            } else {
+                $array['data'] = $this->data[0]->toArray();
+            }
         }
         return $array;
+    }
+
+    public function toJson()
+    {
+        $array = $this->toArray();
+        return json_encode($array);
     }
 }
