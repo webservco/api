ed897db7ca65e1a866e1430bf0510076e9e1defb
Radu Maier <ariademur@webserv.co>
2018-10-31 12:54:19 +0100

    Refactor JsonApi/Resposne

--- src/WebServCo/Api/JsonApi/Response.php
+++ src/WebServCo/Api/JsonApi/Response.php
@@ -4,28 +4,12 @@ namespace WebServCo\Api\JsonApi;
 class Response extends \WebServCo\Framework\HttpResponse
 {
     public function __construct(
-        \WebServCo\Api\JsonApi\Interfaces\ResourceObjectInterface $resourceObject,
-        $errors = [],
-        $statusCode = 200
+        Document $document
     ) {
-        $structure = new Structure();
-        $structure->setData($resourceObject);
-        foreach ($errors as $error) {
-            if ($error instanceof \WebServCo\Api\JsonApi\Error) {
-                $structure->setError($error);
-            }
-        }
-
-        parent::__construct($this->getOutput($structure), $statusCode, ['Content-Type' => Structure::CONTENT_TYPE]);
-    }
-
-    protected function getOutput(Structure $structure)
-    {
-        $jsonOutput = new \WebServCo\Framework\Libraries\JsonOutput();
-        $array = $structure->toArray();
-        foreach ($array as $key => $value) {
-            $jsonOutput->setData($key, $value);
-        }
-        return $jsonOutput->render();
+        parent::__construct(
+            $document->toJson(),
+            $document->getStatusCode(),
+            ['Content-Type' => Document::CONTENT_TYPE]
+        );
     }
 }
