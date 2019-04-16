1896a035869a0e4d9ede1c42c735f459740e550b
Radu Maier <ariademur@webserv.co>
2019-04-16 16:52:25 +0200

    AbstractResponse

---
+++ src/WebServCo/Api/AbstractResponse.php
@@ -0,0 +1,72 @@
+<?php
+namespace WebServCo\Api;
+
+abstract class AbstractResponse
+{
+    protected $endpoint;
+    protected $data;
+    protected $method;
+    protected $response; // \WebServCo\Framework\Http\Response
+    protected $status;
+
+    public function __construct($endpoint, $method, \WebServCo\Framework\Http\Response $response)
+    {
+        $this->endpoint = $endpoint;
+        $this->method = $method;
+        $this->response = $response;
+        $this->status = $this->response->getStatus();
+        $this->data = $this->processResponseData();
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    public function getEndpoint()
+    {
+        return $this->endpoint;
+    }
+
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    protected function processResponseData()
+    {
+        $responseContent = $this->response->getContent();
+        $contentType = $this->response->getHeader('Content-Type');
+        $parts = explode(';', $contentType);
+
+        switch ($parts[0]) {
+            case 'application/json':
+            case 'text/json':
+                return json_decode($responseContent, true);
+                break;
+            case 'application/x-www-form-urlencoded':
+                if (false === strpos($responseContent, '=')) {
+                    /* Sometimes Discogs returns text/plain with this content type ... */
+                    return $responseContent;
+                }
+                $data = [];
+                parse_str($responseContent, $data);
+                return $data;
+                break;
+            case 'text/plain':
+            case 'text/html':
+                return $responseContent;
+                break;
+            default:
+                throw new \WebServCo\Api\Exceptions\ApiException(
+                    sprintf('Api returned unsupported content type: %s.', $contentType)
+                );
+                break;
+        }
+    }
+}
