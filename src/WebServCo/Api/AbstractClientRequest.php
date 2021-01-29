2489266fd8951d08ba3409b501f02ecf2dee37c9
Radu Maier <ariademur@webserv.co>
2021-01-29 16:11:24 +0100

    strict_types

--- src/WebServCo/Api/AbstractClientRequest.php
+++ src/WebServCo/Api/AbstractClientRequest.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace WebServCo\Api;
 
 use WebServCo\Api\JsonApi\Document;
--- src/WebServCo/Api/Exceptions/ApiException.php
+++ src/WebServCo/Api/Exceptions/ApiException.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace WebServCo\Api\Exceptions;
 
 class ApiException extends \WebServCo\Framework\Exceptions\HttpException
